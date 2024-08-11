d8f37c1b5362206f3ecc7dc2fb98192fe2500a74
dboo0012 <dboo0012@student.monash.edu>
2024-08-11 11:22:13 +0800

    add navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Table from "./table/Table";
 import { useState, useEffect } from "react";
+import Navbar from "./ui/Navbar";
 
 function Home(){
   
   return (
     <div>
+      <Navbar />
       <Table apiKey="/api/posts"/>
     </div>
   );
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="navbar bg-primary rounded-xl mt-2 mb-2">
+            <a className="btn btn-ghost text-xl text-slate-800">Books</a>
+        </div>
+    );
+    }
+
+export default Navbar;
